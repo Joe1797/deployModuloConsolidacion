d51bf7521d72178417694fcaa0133a546e10aae8
Joe <joelfabrizio17@gmail.com>
2023-05-02 21:54:49 -0500

    V7

--- api/src/database/db.js
+++ api/src/database/db.js
@@ -6,8 +6,8 @@ const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } = process.env;
 
 const sequelize = new Sequelize(
   // `postgres://ezqtlpug:L_bNaTsOWLaWFGPT3BaShfLoGzy3HT90@lallah.db.elephantsql.com/ezqtlpug`,
-  // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
   {
     logging: false,
   }
